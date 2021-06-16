df28fc11009620ebee855ff185aeaa48597b9dd1
Saget Amerta <sagetamerta@gmail.com>
2021-06-17 00:00:31 +0800

    Clear unused

--- src/Penyebrangan_Kapal/InsertKromosom.java
+++ src/Penyebrangan_Kapal/InsertKromosom.java
@@ -1,6 +1,5 @@
 package Penyebrangan_Kapal;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -37,15 +36,15 @@ public class InsertKromosom extends JFrame {
 	private JTextField txtGen_10;
 	private JComboBox cbKromosom;
 
-	SQL_Connection koneksi= new SQL_Connection();
-	Connection con=koneksi.getKoneksi();
+	SQL_Connection koneksi = new SQL_Connection();
+	Connection con = koneksi.getKoneksi();
 	Statement st;
 	PreparedStatement ps;
-	
+
 	Kromosom kro[];
-	DefaultComboBoxModel<String> cbModel= new DefaultComboBoxModel<String>();
+	DefaultComboBoxModel<String> cbModel = new DefaultComboBoxModel<String>();
 	private JTextField txtNoKromosom;
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -79,61 +78,60 @@ public class InsertKromosom extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblPilihKromosom = new JLabel("Pilih Kromosom");
 		lblPilihKromosom.setBounds(45, 44, 104, 14);
 		contentPane.add(lblPilihKromosom);
-		
+
 		cbKromosom = new JComboBox();
 		cbKromosom.setBounds(159, 41, 139, 20);
 		contentPane.add(cbKromosom);
 		cbKromosom.setModel(cbModel);
-		
-		
+
 		JLabel lblIsiKromosomsel = new JLabel("Isi Kromosom (Gen)");
 		lblIsiKromosomsel.setBounds(45, 72, 139, 14);
 		contentPane.add(lblIsiKromosomsel);
-		
+
 		JLabel label = new JLabel("1.");
 		label.setBounds(45, 101, 22, 14);
 		contentPane.add(label);
-		
+
 		JLabel label_1 = new JLabel("2.");
 		label_1.setBounds(45, 132, 22, 14);
 		contentPane.add(label_1);
-		
+
 		JLabel label_2 = new JLabel("4.");
 		label_2.setBounds(45, 192, 22, 14);
 		contentPane.add(label_2);
-		
+
 		JLabel label_3 = new JLabel("3.");
 		label_3.setBounds(45, 162, 22, 14);
 		contentPane.add(label_3);
-		
+
 		JLabel label_4 = new JLabel("5.");
 		label_4.setBounds(45, 224, 22, 14);
 		contentPane.add(label_4);
-		
+
 		JLabel label_5 = new JLabel("6.");
 		label_5.setBounds(45, 254, 22, 14);
 		contentPane.add(label_5);
-		
+
 		JLabel label_6 = new JLabel("7.");
 		label_6.setBounds(45, 284, 22, 14);
 		contentPane.add(label_6);
-		
+
 		JLabel label_7 = new JLabel("8.");
 		label_7.setBounds(45, 314, 22, 14);
 		contentPane.add(label_7);
-		
+
 		JLabel label_8 = new JLabel("9.");
 		label_8.setBounds(45, 343, 22, 14);
 		contentPane.add(label_8);
-		
+
 		JLabel label_9 = new JLabel("10.");
 		label_9.setBounds(42, 373, 22, 14);
 		contentPane.add(label_9);
-		
+
 		txtGen_1 = new JTextField();
 		txtGen_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -143,7 +141,7 @@ public class InsertKromosom extends JFrame {
 		txtGen_1.setBounds(72, 98, 86, 20);
 		contentPane.add(txtGen_1);
 		txtGen_1.setColumns(10);
-		
+
 		txtGen_2 = new JTextField();
 		txtGen_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -153,7 +151,7 @@ public class InsertKromosom extends JFrame {
 		txtGen_2.setColumns(10);
 		txtGen_2.setBounds(72, 128, 86, 20);
 		contentPane.add(txtGen_2);
-		
+
 		txtGen_3 = new JTextField();
 		txtGen_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -163,7 +161,7 @@ public class InsertKromosom extends JFrame {
 		txtGen_3.setBounds(72, 158, 86, 20);
 		contentPane.add(txtGen_3);
 		txtGen_3.setColumns(10);
-		
+
 		txtGen_4 = new JTextField();
 		txtGen_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -173,7 +171,7 @@ public class InsertKromosom extends JFrame {
 		txtGen_4.setColumns(10);
 		txtGen_4.setBounds(72, 189, 86, 20);
 		contentPane.add(txtGen_4);
-		
+
 		txtGen_5 = new JTextField();
 		txtGen_5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -183,7 +181,7 @@ public class InsertKromosom extends JFrame {
 		txtGen_5.setColumns(10);
 		txtGen_5.setBounds(72, 220, 86, 20);
 		contentPane.add(txtGen_5);
-		
+
 		txtGen_6 = new JTextField();
 		txtGen_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -193,7 +191,7 @@ public class InsertKromosom extends JFrame {
 		txtGen_6.setColumns(10);
 		txtGen_6.setBounds(72, 251, 86, 20);
 		contentPane.add(txtGen_6);
-		
+
 		txtGen_7 = new JTextField();
 		txtGen_7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -203,7 +201,7 @@ public class InsertKromosom extends JFrame {
 		txtGen_7.setColumns(10);
 		txtGen_7.setBounds(72, 281, 86, 20);
 		contentPane.add(txtGen_7);
-		
+
 		txtGen_8 = new JTextField();
 		txtGen_8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -213,7 +211,7 @@ public class InsertKromosom extends JFrame {
 		txtGen_8.setColumns(10);
 		txtGen_8.setBounds(72, 311, 86, 20);
 		contentPane.add(txtGen_8);
-		
+
 		txtGen_9 = new JTextField();
 		txtGen_9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -223,18 +221,18 @@ public class InsertKromosom extends JFrame {
 		txtGen_9.setColumns(10);
 		txtGen_9.setBounds(72, 340, 86, 20);
 		contentPane.add(txtGen_9);
-		
+
 		txtGen_10 = new JTextField();
 		txtGen_10.setColumns(10);
 		txtGen_10.setBounds(72, 370, 86, 20);
 		contentPane.add(txtGen_10);
-		
+
 		JButton btnSimpan = new JButton("Simpan");
 		btnSimpan.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				ResultSet rs;
 				try {
-					ps=con.prepareStatement("INSERT INTO kromosom VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+					ps = con.prepareStatement("INSERT INTO kromosom VALUES(?,?,?,?,?,?,?,?,?,?,?)");
 					ps.setString(1, (txtNoKromosom.getText()));
 					ps.setString(2, (txtGen_1.getText().toString()));
 					ps.setString(3, (txtGen_2.getText().toString()));
@@ -246,20 +244,20 @@ public class InsertKromosom extends JFrame {
 					ps.setString(9, (txtGen_8.getText().toString()));
 					ps.setString(10, (txtGen_9.getText().toString()));
 					ps.setString(11, (txtGen_10.getText().toString()));
-					
+
 					ps.executeUpdate();
-					
+
 					cbKromosom.removeAllItems();
 					getKromosom();
 					reset();
-				}catch (Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 		btnSimpan.setBounds(202, 129, 89, 23);
 		contentPane.add(btnSimpan);
-		
+
 		JButton btnPrint = new JButton("Print");
 		btnPrint.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -268,7 +266,7 @@ public class InsertKromosom extends JFrame {
 		});
 		btnPrint.setBounds(202, 159, 89, 23);
 		contentPane.add(btnPrint);
-		
+
 		JButton btnReset = new JButton("Reset");
 		btnReset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -277,50 +275,50 @@ public class InsertKromosom extends JFrame {
 		});
 		btnReset.setBounds(202, 188, 89, 23);
 		contentPane.add(btnReset);
-		
+
 		txtNoKromosom = new JTextField();
 		txtNoKromosom.setBounds(211, 10, 86, 20);
 		contentPane.add(txtNoKromosom);
 		txtNoKromosom.setColumns(10);
-		
+
 		JLabel lblMasukkanNoKomosom = new JLabel("Masukkan No. Komosom");
 		lblMasukkanNoKomosom.setBounds(45, 13, 155, 14);
 		contentPane.add(lblMasukkanNoKomosom);
 	}
+
 	private void getKromosom() {
 		try {
 			ResultSet rs;
-			String sql="SELECT no_kromosom FROM kromosom "
-					+ "ORDER BY no_kromosom ";
-			st=con.createStatement();
-			rs=st.executeQuery(sql);
-			
+			String sql = "SELECT no_kromosom FROM kromosom " + "ORDER BY no_kromosom ";
+			st = con.createStatement();
+			rs = st.executeQuery(sql);
+
 			rs.last();
 			int jumlahData = rs.getRow();
-			kro=new Kromosom[jumlahData];
-			
+			kro = new Kromosom[jumlahData];
+
 			rs.beforeFirst();
-			int i=0;
-			while(rs.next()) {
-				kro[i]=new Kromosom(rs.getString(1));
+			int i = 0;
+			while (rs.next()) {
+				kro[i] = new Kromosom(rs.getString(1));
 				cbModel.addElement(String.valueOf(kro[i].getKode()));
 				i++;
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void print() {
 		ResultSet rs;
 		try {
-			ps=con.prepareStatement("SELECT * FROM kromosom WHERE no_kromosom=?");
-			ps.setString(1,cbKromosom.getSelectedItem().toString());
-			rs=ps.executeQuery();
-			
-			if(rs.next()) {
-				System.out.println("Kromosom "+ rs.getString(1) +" || Isinya : ");
+			ps = con.prepareStatement("SELECT * FROM kromosom WHERE no_kromosom=?");
+			ps.setString(1, cbKromosom.getSelectedItem().toString());
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				System.out.println("Kromosom " + rs.getString(1) + " || Isinya : ");
 				System.out.println(rs.getString(2));
 				System.out.println(rs.getString(3));
 				System.out.println(rs.getString(4));
@@ -332,11 +330,12 @@ public class InsertKromosom extends JFrame {
 				System.out.println(rs.getString(10));
 				System.out.println(rs.getString(11));
 			}
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	public void reset() {
 		txtGen_1.setText("");
 		txtGen_2.setText("");
